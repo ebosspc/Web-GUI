0e8d4326daf3abe9c9939af9e57dc7b637a883db
Ethan Francolla <e.francolla@gmail.com>
2022-03-13 14:01:55 -0400

    added comments to initial code

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
+# Import necessary libraries
 import subprocess
 import tkinter as tk
 import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
+# Define a function to do a specied command
 def do_command():
     subprocess.call("ping localhost")
 
+# Add a frame
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-# set up button to run the do_command function
+# Set up button to run the do_command function
 ping_btn = tk.Button(frame, text="ping", command=do_command)
 ping_btn.pack()
 
+# Make the GUI persistent
 root.mainloop()
