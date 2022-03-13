c7c2b3dc646ba2b21db59fc35684520fd972a6c1
Ethan Francolla <e.francolla@gmail.com>
2022-03-13 13:43:53 -0400

    initial commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import subprocess
+import tkinter as tk
+import tkinter.scrolledtext as tksc
+from tkinter import filedialog
+from tkinter.filedialog import asksaveasfilename
+
+def do_command():
+    subprocess.call("ping localhost")
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+# set up button to run the do_command function
+ping_btn = tk.Button(frame, text="ping", command=do_command)
+ping_btn.pack()
+
+root.mainloop()
